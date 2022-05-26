9f9b7df8b84a2869b69d32c1287e012e40239803
imhilla <hillaryodhiambo282@gmail.com>
2022-05-26 04:37:06 +0300

    three types of DI classes

--- src/main/java/com/hillary/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/hillary/cardatabase/CardatabaseApplication.java
@@ -14,3 +14,20 @@ public class CardatabaseApplication {
 		logger.info("Cardatabase started, we are tracking the app");
 	}
 };
+
+/**
+ * Dependency injection is a software development technique where we can create
+ * objects
+ * that depend on other objects. DI helps with interaction between classes but
+ * at some time keeps the classes independent.
+ * 
+ * There are three types of classes in DI
+ * 1. Service (is a class that can be used, this is the dependency)
+ * 2. Client (class that uses the dependency)
+ * 3. Injector (passes the dependency (service) to the dependent client)
+ * 
+ * DI makes classes loosely coupled. This means that the creation of client
+ * dependencies is separated from client's behavior, which makes unit testing
+ * easier.
+ * 
+ */
