b472fe7f82117f5654006496dd4428f6455da9be
imhilla <hillaryodhiambo282@gmail.com>
2022-05-31 05:25:04 +0300

    inject car repository

--- src/main/java/com/hillary/cardatabase/web/CarController.java
+++ src/main/java/com/hillary/cardatabase/web/CarController.java
@@ -1,8 +1,19 @@
 package com.hillary.cardatabase.web;
 
+import com.hillary.cardatabase.domain.Car;
+import com.hillary.cardatabase.domain.CarRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CarController {
-  
+  @Autowired
+  private CarRepository repository;
+
+  @RequestMapping("/cars")
+  public Iterable<Car> getCars() {
+    return repository.findAll();
+  }
 }
